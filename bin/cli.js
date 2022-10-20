7524773a635252388c0012a0bc58cbe0e4c152cd
赵振华 <zhenhua.zhao@metalpha.finance>
2022-10-20 17:52:59 +0800

    feat: 支持Crowdin V2的APi

--- bin/cli.js
+++ bin/cli.js
@@ -1,7 +1,7 @@
 let program = require('commander');
 const pkg = require('../package.json');
 const chalk = require('chalk');
-const Crowdin = require('../lib/crowdin');
+const CrowdinV2 = require('../lib/crowdin.v2');
 
 program
     .usage('fed-crowdin-cli')
@@ -12,7 +12,7 @@ program
     .command('pull [lang]')
     .description('Pull translations. When there are no parameters, pull all translations.')
     .action(function (args,otherArgs,cmd) {
-        const crowdin = new Crowdin();
+        const crowdin = new CrowdinV2();
         crowdin.pull(cmd.args);
     });
 
@@ -20,7 +20,7 @@ program
     .command('push')
     .description('Push the entries in the current branch change code')
     .action(function (args,otherArgs,cmd) {
-        const crowdin = new Crowdin();
+        const crowdin = new CrowdinV2();
         crowdin.push();
     });
 
